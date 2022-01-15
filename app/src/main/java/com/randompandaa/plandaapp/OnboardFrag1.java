f9519c44c9b478b0ce57e9bde40cb808b9b7ae02
RandomPandaa <aidan.maher@hotmail.com>
2022-01-15 03:35:25 +0000

    update
    All intro tabs have functioning skip option. last tab has enter button. all successfully working now.

--- app/src/main/java/com/randompandaa/plandaapp/OnboardFrag1.java
+++ app/src/main/java/com/randompandaa/plandaapp/OnboardFrag1.java
@@ -1,5 +1,6 @@
 package com.randompandaa.plandaapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,11 +15,27 @@ import androidx.fragment.app.Fragment;
 
 public class OnboardFrag1 extends Fragment {
 
+    private TextView skipTxt;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        ViewGroup root = (ViewGroup)inflater.inflate(R.layout.fragment_onboarding1, container, false);
-        return root;
+        ViewGroup view = (ViewGroup)inflater.inflate(R.layout.fragment_onboarding1, container, false);
+
+        TextView skipTxt = view.findViewById(R.id.skip_frag1_header);
+
+        skipTxt.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Intro Info Skipped", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getActivity(), MainActivity.class);
+                startActivity(intent);
+
+
+            }
+        });
+
+        return view;
 
     }
 
--- app/src/main/java/com/randompandaa/plandaapp/OnboardFrag3.java
+++ app/src/main/java/com/randompandaa/plandaapp/OnboardFrag3.java
@@ -1,20 +1,41 @@
 package com.randompandaa.plandaapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class OnboardFrag3 extends Fragment {
+
+    private Button enterBtn;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        ViewGroup root = (ViewGroup)inflater.inflate(R.layout.fragment_onboarding3, container, false);
-        return root;
+        ViewGroup view = (ViewGroup)inflater.inflate(R.layout.fragment_onboarding3, container, false);
+
+        Button enterBtn = view.findViewById(R.id.enterAppBtn);
+
+        enterBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "App Entered", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getActivity(), MainActivity.class);
+                startActivity(intent);
+
+
+            }
+        });
+
+        return view;
     }
 
     public void onClick(View view){
