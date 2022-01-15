fce1d8ea9feadf8624a5d48176c8b3ed3da6108d
RandomPandaa <aidan.maher@hotmail.com>
2022-01-15 04:15:00 +0000

    update .3
    test update - my need to revert

--- app/src/main/java/com/randompandaa/plandaapp/LoginTabFrag.java
+++ app/src/main/java/com/randompandaa/plandaapp/LoginTabFrag.java
@@ -2,7 +2,6 @@ package com.randompandaa.plandaapp;
 
 
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,50 +9,48 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 public class LoginTabFrag extends Fragment {
 
-//    private FirebaseAuth mAuth;
-//    private EditText email, password;
-//    private Button btnLogin;
-//    private TextView textLogin;
-//
-//
-//    @Nullable
-//    @Override
-//    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-//        ViewGroup root = (ViewGroup)inflater.inflate(R.layout.login_tab_frag, container, false);
-//
-//
-//        mAuth = FirebaseAuth.getInstance();
-//        email = email.findViewById(R.id.login_email_txt);
-//        password = password.findViewById(R.id.login_password_txt);
-//        btnLogin = btnLogin.findViewById(R.id.login_btn);
-//
-//        btnLogin.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                login();
-//            }
-//        });
-//
-//
-//        return root;
-//
-//
-//
-//    }
+    private FirebaseAuth mAuth;
+    private EditText email, password;
+    private Button btnLogin;
+    private TextView textLogin;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup view = (ViewGroup) inflater.inflate(R.layout.login_tab_frag, container, false);
+
+
+        mAuth = FirebaseAuth.getInstance();
+        email = email.findViewById(R.id.login_email_eTxt);
+        password = password.findViewById(R.id.login_password_txt);
+        btnLogin = btnLogin.findViewById(R.id.login_btn);
+
+
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //login();
+            }
+        });
+
+
+        return view;
+}
+
+
+
+    }
 //
 //    private void login() {
 //        String user = email.getText().toString().trim();
@@ -69,8 +66,9 @@ public class LoginTabFrag extends Fragment {
 //                @Override
 //                public void onComplete(@NonNull Task<AuthResult> task) {
 //                    if(task.isSuccessful()){
-//                        Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
-//                        startActivity(new Intent(LoginActivity.this, MainActivity.class));
+//                        Toast.makeText(getActivity(), "Login Successful", Toast.LENGTH_SHORT).show();
+//                        Intent intent = new Intent(getActivity(), MainActivity.class);
+//                        startActivity(intent);
 //                    }
 //
 //
@@ -88,9 +86,9 @@ public class LoginTabFrag extends Fragment {
 //            //startActivity
 //        }
 //    }
-
-
-
-
-
-}
+//
+//
+//
+//
+//
+//}
