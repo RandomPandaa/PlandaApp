44f098c6201a2ad4a82e29890cd3c6bba33b4087
RandomPandaa <aidan.maher@hotmail.com>
2022-01-16 16:22:26 +0000

    update
    bugs not fixed last, time, now fixed (test).

--- app/src/main/java/com/randompandaa/plandaapp/LoginTabFrag.java
+++ app/src/main/java/com/randompandaa/plandaapp/LoginTabFrag.java
@@ -2,26 +2,35 @@ package com.randompandaa.plandaapp;
 
 
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.concurrent.Executor;
 
 public class LoginTabFrag extends Fragment {
 
     private FirebaseAuth mAuth;
     private EditText email, password;
     private Button btnLogin;
-    private TextView textLogin, swipe;
+    private TextView textLogin;
     float v=0;
 
 
@@ -31,38 +40,24 @@ public class LoginTabFrag extends Fragment {
         ViewGroup view = (ViewGroup) inflater.inflate(R.layout.login_tab_frag, container, false);
 
 
-<<<<<<< Updated upstream
-=======
         btnLogin = view.findViewById(R.id.login_btn);
         email = view.findViewById(R.id.login_email_eTxt);
         password = view.findViewById(R.id.login_password_eTxt);
-        swipe = view.findViewById(R.id.swipe_txt);
 
         // Firebase Auth
->>>>>>> Stashed changes
         mAuth = FirebaseAuth.getInstance();
-        email = email.findViewById(R.id.login_email_eTxt);
-        password = password.findViewById(R.id.login_password_txt);
-        btnLogin = btnLogin.findViewById(R.id.login_btn);
+
+
 
 
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-<<<<<<< Updated upstream
-                //login();
-            }
-        });
-
-
-        return view;
-}
-
-=======
                 String sEmail = email.getText().toString();
                 String sPassword = password.getText().toString();
-                mAuth.signInWithEmailAndPassword(sEmail, sPassword).addOnCompleteListener((Executor) LoginTabFrag.this, new OnCompleteListener<AuthResult>() {
+                mAuth.signInWithEmailAndPassword(sEmail, sPassword)
+                        .addOnCompleteListener((Executor) LoginTabFrag.this, new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
                                 if (task.isSuccessful()) {
@@ -82,73 +77,35 @@ public class LoginTabFrag extends Fragment {
         email.setTranslationY(300);
         btnLogin.setTranslationY(300);
         password.setTranslationY(300);
-        swipe.setTranslationY(300);
 
         email.setAlpha(v);
         btnLogin.setAlpha(v);
         password.setAlpha(v);
-        swipe.setAlpha(v);
 
         email.animate().translationY(0).alpha(1).setDuration(800).setStartDelay(300).start();
         btnLogin.animate().translationY(0).alpha(1).setDuration(800).setStartDelay(500).start();
         password.animate().translationY(0).alpha(1).setDuration(800).setStartDelay(300).start();
-        swipe.animate().translationY(0).alpha(1).setDuration(800).setStartDelay(300).start();
-
-
+        
         return view;
 
 
 
 
     }
->>>>>>> Stashed changes
 
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser == null) {
+            Toast.makeText(getActivity(), "Current User is Null", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(getActivity(), "Current User is " + currentUser.getEmail(), Toast.LENGTH_SHORT).show();
+            //updateUI(currentUser);
+        }
     }
-<<<<<<< Updated upstream
-//
-//    private void login() {
-//        String user = email.getText().toString().trim();
-//        String pass = password.getText().toString().trim();
-//        if(user.isEmpty()){
-//            email.setError("No Email Provided..");
-//        }
-//        if(pass.isEmpty()){
-//            password.setError("No Password Provided..");
-//        }
-//        else{
-//            mAuth.signInWithEmailAndPassword(user,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-//                @Override
-//                public void onComplete(@NonNull Task<AuthResult> task) {
-//                    if(task.isSuccessful()){
-//                        Toast.makeText(getActivity(), "Login Successful", Toast.LENGTH_SHORT).show();
-//                        Intent intent = new Intent(getActivity(), MainActivity.class);
-//                        startActivity(intent);
-//                    }
-//
-//
-//                }
-//            });
-//        }
-//    }
-//
-//
-//
-//    public void onStart() {
-//        super.onStart();
-//        FirebaseUser currentUser = mAuth.getCurrentUser();
-//        if(currentUser == null){
-//            //startActivity
-//        }
-//    }
-//
-//
-//
-//
-//
-//}
-=======
 
 
 }
->>>>>>> Stashed changes
--- app/src/main/java/com/randompandaa/plandaapp/RegisterTabFrag.java
+++ app/src/main/java/com/randompandaa/plandaapp/RegisterTabFrag.java
@@ -1,5 +1,14 @@
 package com.randompandaa.plandaapp;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
